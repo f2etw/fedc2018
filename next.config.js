5312cb918c323ee8bec621b8e51aecb7a774de05
YuTin <yuting1987@gmail.com>
2018-03-03 00:04:58 +0800

    fix(web): added next.config.js for export

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+  }),
+  webpack: (config) => {
+    // Fixes npm packages that depend on `fs` module
+    config.node = { fs: 'empty' };
+
+    return config;
+  },
+};
