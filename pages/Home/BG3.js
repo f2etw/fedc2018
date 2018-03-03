ea25816ccd9bc2892fc05019fbcb891b6e9293de
JerryHong <s6323859@hotmail.com>
2018-03-03 23:02:30 +0800

    close #15

---
+++ pages/Home/BG3.js
@@ -0,0 +1,48 @@
+import React, { PureComponent } from 'react';
+
+class BG3 extends PureComponent {
+  render() {
+    return (
+      <svg width="38.75rem" height="15.5rem" viewBox="0 0 620 248" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <filter
+            x="-27.9%"
+            y="-35.6%"
+            width="155.7%"
+            height="171.1%"
+            filterUnits="objectBoundingBox"
+            id="a"
+          >
+            <feGaussianBlur stdDeviation="10" in="SourceGraphic" />
+          </filter>
+        </defs>
+        <g transform="rotate(-180 314.5 116)" fill="none" fillRule="evenodd">
+          <rect
+            stroke="#02F694"
+            strokeWidth="3"
+            opacity=".3"
+            transform="rotate(-15 229.76 141.524)"
+            x="5.166"
+            y="100.854"
+            width="449.189"
+            height="81.341"
+            rx="40.67"
+          />
+          <rect
+            fill="#02F694"
+            opacity=".7"
+            filter="url(#a)"
+            transform="rotate(-15 551.017 54.676)"
+            x="497.196"
+            y="12.506"
+            width="107.642"
+            height="84.341"
+            rx="42.17"
+          />
+        </g>
+      </svg>
+    );
+  }
+}
+
+export default BG3;
---
+++ pages/Home/BG5.js
@@ -0,0 +1,41 @@
+import React, { PureComponent } from 'react';
+
+class BG5 extends PureComponent {
+  render() {
+    return (
+      <svg width="19.8rem" height="6.8rem" viewBox="0 0 317 109" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(-3 -3)" fill="#594A9D" fillRule="evenodd">
+          <rect
+            opacity=".8"
+            transform="rotate(-15 184.188 51.696)"
+            x="93.595"
+            y="36.696"
+            width="181.185"
+            height="30"
+            rx="15"
+          />
+          <rect
+            opacity=".5"
+            transform="rotate(-15 44.835 89.349)"
+            x="2.875"
+            y="74.349"
+            width="83.92"
+            height="30"
+            rx="15"
+          />
+          <rect
+            opacity=".3"
+            transform="rotate(-15 301.421 19.356)"
+            x="283.264"
+            y="4.356"
+            width="36.316"
+            height="30"
+            rx="15"
+          />
+        </g>
+      </svg>
+    );
+  }
+}
+
+export default BG5;
