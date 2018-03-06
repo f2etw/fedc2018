276a3f9a7c11cb4ed5ca616b2067e078c39d90b7
CJies <cijies@gmail.com>
2018-03-06 22:29:39 +0800

    Adjust <Footer> styles to fit mobile view

--- components/Date.js
+++ components/Date.js
@@ -4,20 +4,27 @@ import styled from 'styled-components';
 const Month = styled.div`
   font-weight: bold;
   font-size: 1.5rem;
-  line-height: 2rem;
   letter-spacing: 0.13rem;
+
+  @media (max-width: 768px) {
+    display: inline;
+  }
 `;
 
 const Year = styled.div`
   font-size: 1.25rem;
-  line-height: 1.5rem;
-  line-height: 1.5rem;
   letter-spacing: 0.1rem;
+  margin-top: 0.5rem;
+
+  @media (max-width: 768px) {
+    display: inline;
+    font-size: 1.5rem;
+  }
 `;
 
 const Date = () => (
   <time dateTime="2018-07-17 09:00">
-    <Month>14 JUL</Month>
+    <Month>14 JUL&nbsp;</Month>
     <Year>2018</Year>
   </time>
 );
