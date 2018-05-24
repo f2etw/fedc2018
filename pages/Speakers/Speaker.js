d3c2ab048c9acdfe2bee3068b48bc860443b4914
wl00887404 <wl00887404@gmail.com>
2018-05-24 10:52:06 +0800

    refactor(web): make /speakers more in line with the design

---
+++ pages/Speakers/Speaker.js
@@ -0,0 +1,77 @@
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.div`
+  width: 18em;
+  &:not(:nth-child(3n)){
+    margin-right: 5.13em;
+  }
+
+  &:nth-child(n+4){
+    margin-top: 6.25em;
+  }
+`;
+
+const Avatar = styled.img`
+  width: 12.5rem;
+  height: 12.5rem;
+  border-radius: 50%;
+  margin:0 2.75rem;
+  margin-bottom: 1.9rem;
+  display: block;
+  filter: grayscale(100%);
+  transition: filter 300ms;
+  cursor: pointer;
+
+  &:hover{
+    filter: grayscale(0%);    
+  }
+`;
+
+const Name = styled.div`
+  font-size: 1.5rem;
+  text-align: center;
+  line-height: 1;
+  margin-bottom: 1.25rem;
+  cursor: pointer;  
+`;
+
+const Title = styled.pre`
+  text-align: center;
+  font-size: 1rem;
+  line-height: 1.25rem;
+  color: #C2B9EE;
+  font-weight: 500;
+  
+  &:not(:last-of-type){
+    margin-bottom: 1.15em;  
+  }
+`;
+
+export default class Speaker extends PureComponent {
+  static propTypes = {
+    name: PropTypes.string,
+    avatar: PropTypes.string,
+    titles: PropTypes.arrayOf(PropTypes.string),
+  }
+
+  static defaultProps={
+    name: '',
+    avatar: '',
+    titles: [],
+  }
+
+  renderTitle = title => <Title key={title} dangerouslySetInnerHTML={{ __html: title }} />
+
+  render() {
+    const { name, avatar, titles } = this.props;
+    return (
+      <Wrapper>
+        <Avatar src={avatar} />
+        <Name>{name}</Name>
+        {titles.map(this.renderTitle)}
+      </Wrapper>
+    );
+  }
+}
--- pages/speakers.js
+++ pages/speakers.js
@@ -1,52 +1,43 @@
-import _ from 'lodash';
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import withMenu from './Layout/withMenu';
-import withBackground from './Layout/withBackground';
-import Speaker from '../components/Speaker';
+import withFedcBackground from './Layout/withFedcBackground';
+import Speaker from './Speakers/Speaker';
+import data from './Speakers/data.json';
 
 const Wrapper = styled.div`
-  width: 100%;
-  top: 0px;
-  left: 0;
-  z-index: 100;
-  margin-top: 120px;
-  padding-bottom: 120px;
+  flex-grow: 1;
+  width: 64.26rem;
+  margin:9.88rem auto 16.75rem auto;
 `;
 
-const Items = styled.div`
-  width: 100%;
-  max-width: 980px;
-  margin: 0 auto;
+const Title = styled.h1`
+  font-size: 1.88rem;
+  letter-spacing: 1rem;
+  margin-left: 0.42rem;
+  margin-bottom: 6.25rem;
+`;
+
+const Container = styled.div`
   display: flex;
-  justify-content: space-between;
   flex-wrap: wrap;
 `;
 
-const items = [
-  { avatar: '/static/speaker/blue_chen.png', name: 'Blue Chen', title: 'RelaJet 洞見未來創辦人' },
-  { avatar: '/static/speaker/ct_wu.png', name: 'CT Wu', title: 'Frontend Engineer, Appier' },
-  { avatar: '/static/speaker/kevin_tu.png', name: '屠澤寬 Jeremiah Kevin Tu', title: 'Sr. Front End Engineer' },
-  { avatar: '/static/speaker/david_khourshid.png', name: 'David Khourshid', title: 'Software Engineer, Microsoft' },
-  { avatar: '/static/speaker/vibert_thio.png', name: '張欣嘉 Vibert Thio', title: '新媒體藝術家' },
-  { avatar: '/static/speaker/johny_vino.png', name: 'Johny Vino', title: 'School of Visual Arts, MFA Interaction design, New York' },
-  { avatar: '/static/speaker/diane_shen.png', name: '沈美君 Diane Shen', title: 'Creative Designer / Strategy Designer, Business Models Inc Taiwan' },
-  { avatar: '/static/speaker/nelson_kuo.png', name: '郭正澔 Nelson Kuo', title: 'TANG Taipei office Sr. Manager / Sr. Experience Design Researcher. Conversion Lab Co-Founder' },
-  { avatar: '/static/speaker/mengchih_chiang.png', name: '江孟芝 MengChih Chiang', title: 'Faculty, MFA Computer Art Department, School of Visual Arts. Creative Director, Mengdom Experimental Design Lab' },
-  { avatar: '/static/speaker/chris_chen.png', name: '陳偉仁 Chris Chen', title: 'Product Design, PicCollage' },
-  { avatar: '/static/speaker/hsienhui_tang.png', name: '唐玄輝 Hsien-Hui Tang', title: 'Professor of Design Department NTUST. User Experience Innovation Consultant at DITLDESIGN & DBAcademy' },
-];
-
 class Speakers extends PureComponent {
+  renderSpeaker = ({ name, avatar, titles }) => (
+    <Speaker name={name} avatar={avatar} titles={titles} />
+  )
+
   render() {
     return (
       <Wrapper>
-        <Items>
-          {_.sortBy(items, ['avatar']).map(attrs => <Speaker {...attrs} />)}
-        </Items>
+        <Title>SPEAKERS</Title>
+        <Container>
+          {data.map(this.renderSpeaker)}
+        </Container>
       </Wrapper>
     );
   }
 }
 
-export default withMenu(withBackground(Speakers));
+export default withMenu(withFedcBackground(Speakers));
